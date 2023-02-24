5767af909c1ae6cc436d4cf9a516e01824460b8f
shikha-knoxpo <shikha.hotwani@knpxpo.com>
2023-02-24 13:19:46 +0530

    login page-web done

--- src/components/BackGround/index.jsx
+++ src/components/BackGround/index.jsx
@@ -1,19 +1,24 @@
 import classes from "./style.module.css";
-import backImg from "./asset/Saly-3.png"; 
+import backImg from "./asset/Saly-3.png";
 import rightImg from "./asset/Saly-2.png";
 import { FormComponent } from "../FormComponent";
-export const BackGround = () =>{
-    return(<>
-    <div className={classes.main} >
-        <div className={classes.left}>
-            <img className={classes.leftimg}  src={backImg}></img>
+export const BackGround = () => {
+  return (
+    <>
+      <div className={classes.main}>
+        <div className={classes.left} >
+          <img className={classes.leftimg} src={backImg}></img>
+        </div >
+        <div className={classes.place} >
+          <FormComponent />
         </div>
-        <div className={classes.right}> <img className={classes.rightimg}  src={rightImg}></img></div>
-    </div>
-    <div className={classes.place}>
-        <FormComponent/>
-    </div>
-    </>)
-}
+        <div className={classes.right} >
+          {" "}
+          <img className={classes.rightimg} src={rightImg}></img>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default BackGround;
--- src/components/FormComponent/index.jsx
+++ src/components/FormComponent/index.jsx
@@ -1,12 +1,15 @@
 import classes from "./style.module.css";
 import { TextField,Button } from "@mui/material";
+import fb from "./asset/fb.png";
+import apple from "./asset/apple1.png";
+import google from "./asset/g12.png";
 export const FormComponent = () => {
   return (
     <>
       <div className={classes.main}>
         <div className={classes.lefthead}>Welcome to Lorem</div>
         <div className={classes.signin}>Sign in</div>
-        <br />
+        
         <div className={classes.subtitle}>
           Enter your username or email address
         </div>
@@ -15,27 +18,36 @@ export const FormComponent = () => {
             inputProps={{
               style: {
                 height: "10px",
-                width:"420px"
-              },
+                              },
             }}
+            fullWidth
           />
-        </div>
+        </div><br/>
         <div className={classes.subtitle}>Enter password</div>
         <div className={classes.textbox}>
           <TextField
             inputProps={{
               style: {
                 height: "10px",
-                width:"420px"
               },
             }}
-          />
+            fullWidth
+            />
         </div>
         <div className={classes.forgot}>
             Forgot Password?
         </div>
-        <div className={classes.btn}><Button style={{width:"450px",backgroundColor:'#E48700'}} variant="contained" >Sign in</Button></div>
         
+        <div className={classes.btn}><Button fullWidth style={{backgroundColor:'#E48700'}} variant="contained" >Sign in</Button></div>
+        
+        <p className={classes.or}>OR</p>
+        <div style={{display:'flex',gap:'10%'}}>
+            <Button style={{width:'80%',height:'50px',backgroundColor:"#FFF4E3",fontSize:'0.5rem',color:'#B87514'}}><img style={{maxHeight:'30%',maxWidth:'30%',marginRight:'5px'}} src={google}></img>Sign in with google</Button>
+            <Button style={{width:'10%',backgroundColor:"#F7F7F7"}}><img style={{maxHeight:'50%',maxWidth:'50%'}} src={apple}></img></Button>
+            <Button style={{width:'10%',backgroundColor:"#F7F7F7"}}><img style={{maxHeight:'50%',maxWidth:'50%'}} src={fb}></img></Button>
+            
+           </div>
+
       </div>
     </>
   );
